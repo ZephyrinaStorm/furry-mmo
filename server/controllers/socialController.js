68d6d6cff46c76850e2c4b4c73fcdf8978efb7f8
ZephyrinaStorm <96967601+ZephyrinaStorm@users.noreply.github.com>
2026-04-28 18:28:48 -0400

    Create server/controllers/socialController.js file with social management functions

---
+++ server/controllers/socialController.js
@@ -0,0 +1,86 @@
+// socialController.js
+
+/**
+ * Social Management Functions
+ */
+
+/**
+ * Creates a new guild.
+ * @param {string} guildName - The name of the guild.
+ * @returns {Object} - The created guild object.
+ */
+function createGuild(guildName) {
+    // Implementation for creating a guild
+}
+
+/**
+ * Joins an existing guild.
+ * @param {string} guildId - The ID of the guild to join.
+ * @returns {Object} - The joined guild object.
+ */
+function joinGuild(guildId) {
+    // Implementation for joining a guild
+}
+
+/**
+ * Leaves a guild.
+ * @param {string} guildId - The ID of the guild to leave.
+ * @returns {Object} - The left guild object.
+ */
+function leaveGuild(guildId) {
+    // Implementation for leaving a guild
+}
+
+/**
+ * Gets the members of a guild.
+ * @param {string} guildId - The ID of the guild.
+ * @returns {Array} - List of members in the guild.
+ */
+function getGuildMembers(guildId) {
+    // Implementation for getting guild members
+}
+
+/**
+ * Creates a new party.
+ * @param {string} partyName - The name of the party.
+ * @returns {Object} - The created party object.
+ */
+function createParty(partyName) {
+    // Implementation for creating a party
+}
+
+/**
+ * Joins an existing party.
+ * @param {string} partyId - The ID of the party to join.
+ * @returns {Object} - The joined party object.
+ */
+function joinParty(partyId) {
+    // Implementation for joining a party
+}
+
+/**
+ * Leaves a party.
+ * @param {string} partyId - The ID of the party to leave.
+ * @returns {Object} - The left party object.
+ */
+function leaveParty(partyId) {
+    // Implementation for leaving a party
+}
+
+/**
+ * Adds a friend.
+ * @param {string} friendId - The ID of the friend to add.
+ * @returns {Object} - The updated friend list.
+ */
+function addFriend(friendId) {
+    // Implementation for adding a friend
+}
+
+/**
+ * Removes a friend.
+ * @param {string} friendId - The ID of the friend to remove.
+ * @returns {Object} - The updated friend list.
+ */
+function removeFriend(friendId) {
+    // Implementation for removing a friend
+}
